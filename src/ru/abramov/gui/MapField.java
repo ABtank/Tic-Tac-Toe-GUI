42bb95fdb48ba56ba8dfed720355789320e379fd
Abramov Iurii <abtank@outlook.com>
2020-06-04 01:21:33 +0300

    add logic and GUI

--- src/ru/abramov/gui/MapField.java
+++ src/ru/abramov/gui/MapField.java
@@ -2,17 +2,254 @@ package ru.abramov.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class MapField extends JPanel {
 
     public static final int MODE_HVH = 0;
     public static final int MODE_HVA = 1;
 
-    MapField(){
-        setBackground(Color.PINK);
+    private static final int DOT_EMPTY = 0;
+    private static final int DOT_HUMAN = 1;
+    private static final int DOT_AI = 2;
+    private static final int DOT_PADDING = 5;
+
+    private static final Random RANDOM = new Random();
+
+    private int stateGameOver;
+    private static final int STATE_DRAW = 0;
+    private static final int STATE_WIN_HUMAN = 1;
+    private static final int STATE_WIN_AI = 2;
+
+    private static final String MSG_WIN_HUMAN = "Победил игрок!";
+    private static final String MSG_WIN_AI = "Победил компьютер!";
+    private static final String MSG_WIN_DRAW = "Ничья!";
+
+    private int[][] field;
+    private int fieldSizeX;
+    private int fieldSizeY;
+    private int winLength;
+    private int cellWidth;
+    private int cellHeight;
+    private boolean isGameOver;
+    private boolean isInitialized;
+    private Color background = Color.PINK;
+
+    MapField() {
+        isInitialized = false;
+        setBackground(background);
     }
 
-    void startNewGame(int gameMode, int fieldSizeX, int fieldSizeY, int winLength ){
-        System.out.printf("mode=%d\nfsx = %d\nwin = %d",gameMode,fieldSizeX,fieldSizeY,winLength);
+    void startNewGame(int gameMode, int fieldSizeX, int fieldSizeY, int winLength) {
+        System.out.printf("mode=%d\nfsx = %d\nwin = %d", gameMode, fieldSizeX, fieldSizeY, winLength);
+        this.fieldSizeX = fieldSizeX;
+        this.fieldSizeY = fieldSizeY;
+        this.winLength = winLength;
+        field = new int[fieldSizeY][fieldSizeX];
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                update(e);
+            }
+        });
+
+        isGameOver = false;
+        isInitialized = true;
+        repaint();
+    }
+
+    private void update(MouseEvent e) {
+        if(!isInitialized) return;
+        if (isGameOver) return;
+        int cellX = e.getX() / cellWidth;
+        int cellY = e.getY() / cellHeight;
+        System.out.printf("x=%d, y=%d\n", cellX, cellY);
+        if (!isValidCell(cellX, cellY) || !isEmptyCell(cellX, cellY)) return;
+        field[cellY][cellX] = DOT_HUMAN;
+        repaint();
+        if (checkWin(DOT_HUMAN)) {
+            setGameOver(STATE_WIN_HUMAN);
+            return;
+        }
+        if (isDraw()) {
+            setGameOver(STATE_DRAW);
+            return;
+        }
+        aiTurn();
+        repaint();
+        if (checkWin(DOT_AI)) {
+            setGameOver(STATE_WIN_AI);
+            return;
+        }
+        if (isDraw()) {
+            setGameOver(STATE_DRAW);
+            return;
+        }
+    }
+
+    private void setGameOver(int gameOverState) {
+        isGameOver = true;
+        stateGameOver = gameOverState;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render(g);
+    }
+
+    private void render(Graphics g) {
+        if(!isInitialized) return;
+        int width = getWidth();
+        int height = getHeight();
+        cellWidth = width / fieldSizeX;
+        cellHeight = height / fieldSizeY;
+        g.setColor(Color.BLACK);
+        for (int i = 0; i < fieldSizeY; i++) {
+            int y = i * cellHeight;
+            g.drawLine(0, y, width, y);
+        }
+        for (int i = 0; i < fieldSizeX; i++) {
+            int x = i * cellWidth;
+            g.drawLine(x, 0, x, height);
+        }
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (isEmptyCell(x, y)) continue;
+                if (field[y][x] == DOT_AI) {
+                    g.setColor(new Color(11, 255, 30));
+                    g.fillOval(x * cellWidth + DOT_PADDING,
+                            y * cellHeight + DOT_PADDING,
+                            cellWidth - DOT_PADDING * 2,
+                            cellHeight - DOT_PADDING * 2);
+                    g.setColor(background);
+                    g.fillOval(x * cellWidth + DOT_PADDING * 4,
+                            y * cellHeight + DOT_PADDING * 4,
+                            cellWidth - DOT_PADDING * 8,
+                            cellHeight - DOT_PADDING * 8);
+                } else if (field[y][x] == DOT_HUMAN) {
+                    g.setColor(new Color(4, 42, 255));
+                    g.fillRect(x * cellWidth + DOT_PADDING,
+                            y * cellHeight + cellHeight / 3,
+                            cellWidth - DOT_PADDING * 2,
+                            cellHeight / 3);
+                    g.fillRect(x * cellWidth + cellWidth / 3,
+                            y * cellHeight + DOT_PADDING,
+                            cellWidth / 3,
+                            cellHeight - DOT_PADDING * 2);
+                } else {
+                    throw new RuntimeException(
+                            String.format("Can't recognize cell field[%d][%d]: %d",
+                                    y, x, field[y][x]));
+                }
+            }
+        }
+        if (isGameOver) {
+            showMessageGameOver(g);
+        }
+    }
+
+    private void showMessageGameOver(Graphics g) {
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(0, 200, getWidth(), 70);
+        g.setColor(Color.YELLOW);
+        g.setFont(new Font("Time new roman", Font.BOLD, 42));
+        switch (stateGameOver) {
+            case STATE_DRAW:
+                g.drawString(MSG_WIN_DRAW, 180, getHeight() / 2);
+                break;
+            case STATE_WIN_AI:
+                g.drawString(MSG_WIN_AI, 20, getHeight() / 2);
+                break;
+            case STATE_WIN_HUMAN:
+                g.drawString(MSG_WIN_HUMAN, 70, getHeight() / 2);
+                break;
+            default:
+                throw new RuntimeException("Unexpected gameOver state: " + stateGameOver);
+        }
+    }
+
+    private boolean isValidCell(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    private boolean isEmptyCell(int x, int y) {
+        return field[y][x] == DOT_EMPTY;
+    }
+
+    private boolean isDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (field[y][x] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    private void aiTurn() {
+        if (turnAIWinCell()) return;
+        if (turnHumanWinCell()) return;
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(x, y));
+        field[y][x] = DOT_AI;
+    }
+
+    private boolean turnAIWinCell() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (isEmptyCell(j, i)) {
+                    field[i][j] = DOT_AI;
+                    if (checkWin(DOT_AI)) return true;
+                    field[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean turnHumanWinCell() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (isEmptyCell(j, i)) {
+                    field[i][j] = DOT_HUMAN;
+                    if (checkWin(DOT_HUMAN)) {
+                        field[i][j] = DOT_AI;
+                        return true;
+                    }
+                    field[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkWin(int c) {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (checkLine(i, j, 1, 0, winLength, c)) return true; //по горизонтали
+                if (checkLine(i, j, 1, 1, winLength, c)) return true; // по диагонали x y
+                if (checkLine(i, j, 0, 1, winLength, c)) return true; // по вертикали
+                if (checkLine(i, j, 1, -1, winLength, c)) return true; // по диаагонали x -y
+            }
+        }
+        return false;
+    }
+
+    private boolean checkLine(int x, int y, int vx, int vy, int len, int c) {
+        final int far_x = x + (len - 1) * vx;
+        final int far_y = y + (len - 1) * vy;
+        if (!isValidCell(far_x, far_y)) return false;
+        for (int i = 0; i < len; i++) {
+            if (field[y + i * vy][x + i * vx] != c) return false;
+        }
+        return true;
     }
 }
